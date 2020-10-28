67301644be13650da9955146efcb188663b8306c
nineey <56111891+nineey@users.noreply.github.com>
2020-10-28 21:12:22 +0100

    first commit

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route("/test")
+def test():
+    return "success"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
