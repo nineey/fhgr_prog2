2aaf378dcd525a2c758ece793d49f1585191ac7c
nineey <56111891+nineey@users.noreply.github.com>
2020-11-14 22:00:00 +0100

    add comments

--- libs/data_handler.py
+++ libs/data_handler.py
@@ -5,32 +5,44 @@ from datetime import date
 date = date.today().strftime('%d.%m.%Y')
 
 
-# create JSON file and add dict
-def save_data(id, deal, price, category):
+def load_data():
+    """
+    gets a dictionary with all deals from (existing) JSON-file
+    :return: dict with all deals
+    """
     try:
-        with open('data/data.json', 'r') as db:
+        with open("data/data.json", 'r') as db:
             deals = json.load(db)
     except:
         deals = {}
+    return deals
 
+
+def save_data(id, deal, price, category):
+    """
+    Saves data of a new deal into new or existing JSON-file
+    :param id: unique ID (given by id_handler)
+    :param deal: name of the deal
+    :param price: price of the deal
+    :param category: category of the deal
+    """
+    deals = load_data()
     deals[id] = {"user": session["USERNAME"], "name": deal, "price": price, "category": category, "date": str(date), "voting": {}}
 
     with open('data/data.json', 'w') as db:
         json.dump(deals, db, indent=4)
 
 
-# load JSON
-def load_data():
-    with open("data/data.json", 'r') as db:
-        deals = json.load(db)
-    return deals
-
-
-# handle unique ID
 def id_handler():
+    """
+    every deal must have a unique ID for identification reasons
+    this function prevents duplicated IDs
+    :return: new (unique) ID
+    """
     try:
         deals = load_data()
         key_list = deals.keys()
+        # list comprehension to form dict_keys into list with integers
         int_key_list = [int(i) for i in key_list]
         new_id = max(int_key_list) + 1
     except:
@@ -40,23 +52,13 @@ def id_handler():
 
 
 def add_voting(deal_id, username, vote):
+    """
+    function to add a user voting to a specific deal and save to JSON
+    :param deal_id: unique id to identify the deal
+    :param username: who has voted?
+    :param vote: accepted or rejected?
+    """
     deals = load_data()
     deals[deal_id]["voting"][username] = vote
     with open('data/data.json', 'w') as db:
         json.dump(deals, db, indent=4)
-
-        """
-
-def vote_accepted(deal_id, username):
-    deals = load_data()
-    deals[deal_id].update(accepted=username)
-    with open('data/data.json', 'w') as db:
-        json.dump(deals, db, indent=4)
-
-
-def vote_rejected(deal_id, username):
-    deals = load_data()
-    deals[deal_id].update(rejected=username)
-    with open('data/data.json', 'w') as db:
-        json.dump(deals, db, indent=4)
-"""
--- libs/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired
-
-
-class EntryForm(FlaskForm):
-    deal_name = StringField('DealName', validators=[DataRequired])
-    deal_price = IntegerField('Price', valdidators=[DataRequired])
